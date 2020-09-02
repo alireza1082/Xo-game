cb14c0305812ad121ccbce67c840d24ec0e03551
alireza <rezaali1387@gmail.com>
2020-09-02 10:20:29 +0430

    generate board game

--- app/src/main/java/com/example/android/xo/MainActivity.java
+++ app/src/main/java/com/example/android/xo/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.android.xo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+
+import java.util.zip.Inflater;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        LayoutInflater inflater = LayoutInflater.from(getApplicationContext());
+//        inflater.inflate();
+        LayoutInflater.from(getApplicationContext()).inflate(R.layout.board , null);
     }
 }
