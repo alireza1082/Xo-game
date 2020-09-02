1295bfd6ec09a7023454b47c4414654dd32e95d9
alireza <rezaali1387@gmail.com>
2020-09-02 11:58:29 +0430

    add o image

--- app/src/main/java/com/example/android/xo/MainActivity.java
+++ app/src/main/java/com/example/android/xo/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.util.zip.Inflater;
 
@@ -14,11 +16,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.board);
-        LayoutInflater inflater = LayoutInflater.from(getApplicationContext());
-        inflater.inflate(R.layout.board , null);
+//        LayoutInflater inflater = LayoutInflater.from(getApplicationContext());
+//        inflater.inflate(R.layout.board , null);
 //        LayoutInflater.from(getApplicationContext()).inflate(R.layout.board , null , true);
     }
-    public void click(){
-
+    public void click(View view){
+        Toast.makeText(getApplicationContext() , "hey" ,Toast.LENGTH_SHORT).show();
+        ImageView image = (ImageView) view;
+        image.setImageResource(R.drawable.o);
     }
 }
