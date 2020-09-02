63c64055226ba1f1370aa23bf50b7b606c1d4e91
alireza <rezaali1387@gmail.com>
2020-09-02 15:33:53 +0430

    add about us page

---
+++ app/src/main/java/com/example/android/xo/AboutUs.java
@@ -0,0 +1,17 @@
+package com.example.android.xo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class AboutUs extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_us);
+    }
+    public void pic(View view){
+    }
+}
--- app/src/main/java/com/example/android/xo/MainActivity.java
+++ app/src/main/java/com/example/android/xo/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -13,13 +15,14 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
+    MenuItem menuItem;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Button button = findViewById(R.id.start);
+            Button button = findViewById(R.id.start);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -27,5 +30,17 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menuItem = menu.add("about us").setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                startActivity(new Intent(MainActivity.this , AboutUs.class));
+                return false;
+            }
+        });
+        menuItem.setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
+        return super.onCreateOptionsMenu(menu);
     }
 }
