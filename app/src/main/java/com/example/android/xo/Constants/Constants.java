e63227a6c7ba12c17a587135c89d468e84bc24e5
alireza <rezaali1387@gmail.com>
2022-10-07 22:27:00 +0330

    Add Constants to project

---
+++ app/src/main/java/com/example/android/xo/Constants/Constants.java
@@ -0,0 +1,6 @@
+package com.example.android.xo.Constants;
+
+public class Constants {
+    public static final String STANDARD_ZONE_ID = "634074967c7871496f5e299a";
+    public static final String VIDEO_ZONE_ID = "6340763d86e87557c99d3dff";
+}
